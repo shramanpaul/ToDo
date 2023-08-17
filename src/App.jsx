4dfa59d0d4468dfd7e4c48a82643418eb8cafcca
shraman <22bcs229@iiitdmj.ac.in>
2023-08-18 00:43:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles.css'
+
+function App() {
+  
+
+  return (
+    <div className="bg-[#adff2f]"> {/* Add a basic Tailwind CSS class */}
+      <h1 className=' text-blue-600'>Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
